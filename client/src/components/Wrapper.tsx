c110a79ad204085cbb34a3972a8a0e314cb2deb6
Leo Le <leolemain@gmail.com>
2021-04-24 16:52:10 -0700

    Init wrapper and input component

---
+++ client/src/components/Wrapper.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+
+interface WrapperProps {}
+
+const Wrapper: React.FC<WrapperProps> = ({}) => {
+  return <Box>
+
+  </Box>;
+};
+
+export default Wrapper;
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface loginProps {}
+
+const login: React.FC<loginProps> = ({}) => {
+  return (
+    <div>
+      <p>Login Page</p>
+    </div>
+  );
+};
+
+export default login;
